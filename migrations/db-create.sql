78e226394502959e22e66e150eede4eb7c024c76
Lino Melhado Neto <linomneto@gmail.com>
2022-04-03 16:36:30 -0300

    creating db table structure for auth

--- migrations/db-create.sql
+++ migrations/db-create.sql
@@ -1,3 +1,15 @@
+CREATE SEQUENCE user_id_seq;
+CREATE TABLE public.user (
+    id int NOT NULL DEFAULT nextval('user_id_seq'),
+    name varchar(200) NOT NULL,
+    email varchar(100) NOT NULL,
+    login varchar(100) NOT NULL,
+    pwd varchar(100) NOT NULL,
+    roles varchar (200) NOT NULL DEFAULT 'USER',
+    CONSTRAINT user_pk PRIMARY KEY (id)
+);
+CREATE UNIQUE INDEX user_id_idx ON public.user USING btree (id); 
+
 CREATE SEQUENCE product_id_seq; 
 CREATE TABLE product ( 
     id int4 NOT NULL DEFAULT nextval('product_id_seq'), 
@@ -6,4 +18,5 @@ CREATE TABLE product (
     brand varchar(100) NULL, 
     CONSTRAINT product_pk PRIMARY KEY (id) 
 ); 
-CREATE UNIQUE INDEX product_id_idx ON public.product USING btree (id); 
+CREATE UNIQUE INDEX product_id_idx ON public.product USING btree (id);
+
--- migrations/db-initial-data-load.sql
+++ migrations/db-initial-data-load.sql
@@ -1,12 +1,13 @@
-INSERT INTO product (description, value, brand) 
-    VALUES('Arroz parboilizado 5Kg', 25, 'Tio João'); 
-INSERT INTO product (description, value, brand) 
-    VALUES('Maionese 250gr', 7.2, 'Helmanns'); 
-INSERT INTO product (description, value, brand) 
-    VALUES('Iogurte Natural 200ml', 2.5, 'Itambé'); 
-INSERT INTO product (description, value, brand) 
-    VALUES('Nescau 400gr', 8, 'Nestlé'); 
-INSERT INTO product (description, value, brand) 
-    VALUES('Batata Palha 180gr', 5.20, 'Chipps'); 
-INSERT INTO product (description, value, brand) 
-    VALUES('Feijão Carioquinha', 5, 'Xap'); 
+INSERT INTO "user" (name, login, pwd, email, roles) VALUES 
+    ('user','user','$2a$08$cgWo0k133AqN7bfHTkKK8eedZRBlpZL08piXXQP521KOFXIF4sWha','user@abc.com.br','USER'),
+    ('admin','admin','$2a$08$cgWo0k133AqN7bfHTkKK8eedZRBlp','admin@abc.com.br','USER;ADMIN')
+;
+
+INSERT INTO product (description, value, brand) VALUES 
+    ('Arroz parboilizado 5Kg', 25,'Tio João'),
+    ('Maionese 250gr', 7.2,'Helmanns'),
+    ('Iogurte Natural 200ml',2.5,'Itambé'),
+    ('Nescau 400gr',8, 'Nestlé'),
+    ('Batata Palha 180gr',5.20,'Chipps'),
+    ('Feijão Carioquinha',5,'Xap')
+;
