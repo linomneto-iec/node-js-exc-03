fe9b49e435c8672c13c5557c0e2918a33ee63617
Lino Melhado Neto <linomneto@gmail.com>
2022-04-03 16:02:43 -0300

    implementing db integration to all products endpoints

--- api/products_api.js
+++ api/products_api.js
@@ -10,80 +10,114 @@ const knex = require('knex')({
       } 
 });
 
-const source = {
-    products: [
-        { id: 1, description: "Arroz parboilizado 5Kg", value: 25.00, brand: "Tio João" },
-        { id: 2, description: "Maionese 250gr", value: 7.20, brand: "Helmans" },
-        { id: 3, description: "Iogurte Natural 200ml", value: 2.50, brand: "Itambé" },
-        { id: 4, description: "Batata Maior Palha 300gr", value: 15.20, brand: "Chipps" },
-        { id: 5, description: "Nescau 400gr", value: 8.00, brand: "Nestlé" },
-    ]
-}
-
 products_api.get('/', (req, res) => {
     knex.select('*').from('product') 
-    .then( products => res.status(200).json(products) ) 
-    .catch(err => { 
-        res.status(500).json({  
-           message: 'error to get products from database -> ' + err.message }) 
-    })
+        .then( products => res.status(200).json(products) ) 
+        .catch(err => { 
+            res.status(500).json({  
+                message: 'error to get products list from database -> ' + err.message 
+            }) 
+        })
 })
 
 products_api.get('/:id', (req, res) => {
     const id = parseInt(req.params.id);
 
-    const product = source.products.find(p => p.id === id)
-    if (!product) {
-        res.status(204).json({ message: "Product not found" })
-        return
-    }
-
-    res.status(200).json(product)
+    knex.select('*').from('product').where({ id: id })
+        .then(products => {
+            if (products.length > 0) {
+                res.status(200).json(products[0])
+            } else {
+                res.status(404).json({ message: "product " + id + " not found" })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error to get product ' + id + ' from database -> ' + err.message 
+            })
+        })
 })
 
 products_api.post('/', (req, res) => {
     const payload = req.body
 
     if (!payload || !payload.description || !payload.value || !payload.brand) {
-        res.status(400).json({ message: "Bad product payload" })
+        res.status(400).json({ message: "bad product payload" })
         return
     }
 
-    let id = (source.products[source.products.length - 1].id) + 1
-    source.products.push({ id, ...payload })
-    res.status(201).json({ message: "Product created successfully" })
+    knex('product')
+        .insert({
+            description: product.description,
+            value: product.value,
+            brand: product.brand,
+        }, ['id'])
+        .then((result) => {
+            res.status(201).json({ 
+                message: "product created successfully",
+                created_id: result[0].id 
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error to create product ' + id + ' into database -> ' + err.message 
+            })
+        })
 })
 
 products_api.put('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
+    const id = parseInt(req.params.id)
     const payload = req.body
 
     if (!payload || !payload.description || !payload.value || !payload.brand) {
-        res.status(400).json({ message: "Bad product payload" })
+        res.status(400).json({ message: "bad product payload" })
         return
     }
 
-    let idx = source.products.findIndex(p => p.id === id)
-    if (idx == -1) {
-        res.status(204).json({ message: "Product not found" })
-        return
-    }
-
-    source.products[idx] = { id, ...payload }
-    res.status(200).json({ message: "Product updated successfully" })
+    knex('product')
+        .update({
+            description: product.description,
+            value: product.value,
+            brand: product.brand,
+        })
+        .where({ 
+            id: id 
+        })
+        .then((n) => {
+            if (n) {
+                res.status(200).json({ 
+                    message: "product updated successfully",
+                    updated_id: id
+                })
+            } else {
+                res.status(204).json({ message: "product id " + id + " not found" })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error to update product ' + id + ' into database -> ' + err.message 
+            })
+        })
 })
 
 products_api.delete('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-
-    let idx = source.products.findIndex(p => p.id === id)
-    if (idx == -1) {
-        res.status(204).json({ message: "Product not found" })
-        return
-    }
-
-    source.products.splice(idx, 1)
-    res.status(204).json({ message: "Product deleted successfully" })
+    const id = req.params.id
+    knex('product')
+        .where({ 
+            id: id 
+        })
+        .del()
+        .then(() => {
+            res.status(200).json({ 
+                message: "product deleted successfully",
+                deleted_id: id 
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error to delete product ' + id + ' in database -> ' + err.message 
+            })
+        })
 })
 
 module.exports = products_api; 
